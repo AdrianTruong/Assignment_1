7a9606127a4aa7f50d3c20df1d1c6f91e146cd70
AdrianTruong <s3872952@student.rmit.edu.au>
2024-04-05 00:05:15 +0700

    Finishing Customer class

--- src/Customer.java
+++ src/Customer.java
@@ -12,5 +12,42 @@ public abstract class Customer {
         id = "Default";
         insuranceCard = "Default";
     }
+    public Customer(String id, String fullName, InsuranceCard insuranceCard) {
+        this.id = id;
+        this.fullName = fullName;
+        this.insuranceCard = insuranceCard;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public InsuranceCard getInsuranceCard() {
+        return insuranceCard;
+    }
+
+    public List<Claim> getClaims() {
+        return claims;
+    }
+
+    // Method to add claims to a customer
+    public void addClaim(Claim claim) {
+        claims.add(claim);
+    }
+
+
+    // Override toString() method
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=' " + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", insuranceCard=" + insuranceCard +
+                '}';
+    }
 
 }
